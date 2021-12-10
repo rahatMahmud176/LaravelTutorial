0372ce5bf36383e619e664cb2e562027a165d0b7
RahatMahmud <rahatmahmud176@gmail.com>
2021-12-11 01:05:12 +0600

    ajax

--- resources/views/ajax-code.php
+++ resources/views/ajax-code.php
@@ -12,3 +12,28 @@
         })
     }) 
 </script>
+
+
+
+ <script>
+     $(document).on('change', '#categoryId', function () {
+        var catId = $(this).val();
+        $.ajax({
+            type: 'GET',
+            url: "{{ url('sub-category-by-category-id') }}",
+            data: {id:catId},
+            dataType: 'json',
+            success: function (res) {
+               // console.log(res);
+                var selectElement = $('#subCategoryId');
+                selectElement.empty();
+                var option = '';
+                option +='<option selected disabled value="">--Select Sub Categroy--</option>';
+                $.each(res, function(key, value){
+                    option +='<option value=" '+value.id+' "> '+value.title+' </option>';
+                })
+                selectElement.append(option);
+            }
+        })
+    }) 
+</script>
